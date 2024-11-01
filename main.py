fb0ffa36d305f38b487284596c8b7ea3dc71210c
Daniel Reis <reisoliveiradaniel360@gmail.com>
2024-11-01 12:24:20 -0300

    Criado funcao de exibicao de mensagem

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ def gether_data():
 
     return n1, n2
 
+def print_message():
+    print(f'Os valores {n1} e {n2} somados dão {n1+n2}')
+
+    return None
 
 def main():
     n1, n2 = gether_data()
     
-    print(n1**n2)
+    print_message(n1, n2)
 
     return None
-
 
 if __name__ == "__main__":
     main()
